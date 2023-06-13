f90ad717076d75605d64681cba5620fe8f2e1e85
Arunava Dutta <121813676+ArunavaCoderEm@users.noreply.github.com>
2023-06-13 20:18:58 +0530

    Update practice.c

--- practice.c
+++ practice.c
@@ -1115,3 +1115,33 @@ int main() {
     printf("\nRequired Sum Of Odd Numbers From An 1D Arrays Is %d",sum);
         return(0);
 }
+
+// // Point on straight line or not
+#include <stdio.h>
+#include<string.h>
+#include <math.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int x1,x2,x3,y1,y2,y3;
+    float m1,m2,m3;
+    printf("Enter Your x1 y1 - ");
+    scanf("%d %d",&x1,&y1);
+    printf("Enter Your x2 y2 - ");
+    scanf("%d %d",&x2,&y2);
+    printf("Enter Your x1 y1 - ");
+    scanf("%d %d",&x3,&y3);
+    m1 = (y2-y1)/(x2-x1);
+    m2 = (y3-y2)/(x3-x2);
+    if (m1 == m2){
+        printf("Lies on Straight line");
+    }
+    else {
+        printf("Not on straight line");
+    }
+        return(0);
+}
