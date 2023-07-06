c0972df87c00ea3bd4d3688d36294feacdb4813b
Arunava Dutta <121813676+ArunavaCoderEm@users.noreply.github.com>
2023-07-06 22:53:51 +0530

    Update practice.c

--- practice.c
+++ practice.c
@@ -1397,3 +1397,40 @@ void SumOfDigit(int n){
     }
     printf("The Sum Of Digits Of %d Is %d",n,sum);
 }
+
+// // // // HACKERRANK FIND Nth TERM
+// // S(n) = S(n-1) + S(n-2) + S(n-3) // // a = S(1) b = S(2) c = S(3)
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+int find_nth_term(int n, int a, int b, int c);
+
+
+
+int main() {
+    int n, a, b, c;
+  
+    scanf("%d %d %d %d", &n, &a, &b, &c);
+    int ans = find_nth_term(n, a, b, c);
+ 
+    printf("%d", ans); 
+    return 0;
+}
+
+int find_nth_term(int n, int a, int b, int c) {
+    if (n == 1){
+        return (a);
+    }
+    else if (n == 2){
+        return (b);
+    }
+    else if (n == 3){
+        return (c);
+    }
+    else {
+        return find_nth_term(n-1,b,c,a+b+c);
+    }
+}
