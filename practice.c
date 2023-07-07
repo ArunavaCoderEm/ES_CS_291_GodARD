ef153e70940a61fef2cc8a6efe23e4d005ae988d
Arunava Dutta <121813676+ArunavaCoderEm@users.noreply.github.com>
2023-07-07 23:26:43 +0530

    Update practice.c

--- practice.c
+++ practice.c
@@ -1434,3 +1434,26 @@ int find_nth_term(int n, int a, int b, int c) {
         return find_nth_term(n-1,b,c,a+b+c);
     }
 }
+
+// // Printing Tokens
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    char *s;
+    s = malloc(1024 * sizeof(char));
+    scanf("%[^\n]", s);
+    s = realloc(s, strlen(s) + 1);
+    for (int i = 0; i < strlen(s); ++i){
+        if (s[i] == ' '){
+            printf("\n");
+        }
+        else {
+            printf("%c",s[i]);
+        }
+    }
+    return 0;
+}
